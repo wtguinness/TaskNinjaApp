3e7405c7e3856e8b664a2ce557b5bc012591ad3e
William Twachtman <wtguinness@aol.com>
2015-03-11 23:40:41 -0400

    +Change Layout to 2 columns +Change edit to Modal +Default index.html to browse.html +Add gravatar and time of author for task posts

---
+++ scripts/controllers/browseController.js
@@ -0,0 +1,3 @@
+app.controller('BrowseController', ['$scope', function ($scope) {
+  
+}});
--- scripts/taskNinjaApp.js
+++ scripts/taskNinjaApp.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var app = angular.module('TaskNinjaApp', ['firebase', 'ngRoute','ngResource','ngAnimate', 'toaster'])
+var app = angular.module('TaskNinjaApp', ['firebase', 'ngRoute','ngResource','ngAnimate', 'toaster', 'angularMoment'])
     .constant('FURL', 'https://blistering-heat-9707.firebaseio.com/')
     .config(function ($routeProvider){
         $routeProvider
       .when('/', {
-           templateUrl: 'views/main.html'
-
-       })
-      .when('/post', {
-           templateUrl: 'views/post.html',
-           controller: 'TaskController'
-       })
-      .when('/edit/:taskId', {
-           templateUrl: 'views/edit.html',
-            controller: 'TaskController'
-       })
-      .when('/browse', {
            templateUrl: 'views/browse.html',
-            controller: 'TaskController'
+            controller: 'BrowseController'
+       })
+//      .when('/post', {
+//           templateUrl: 'views/post.html',
+//           controller: 'TaskController'
+//       })
+//      .when('/edit/:taskId', {
+//           templateUrl: 'views/edit.html',
+//            controller: 'TaskController'
+//       })
+      .when('/browse/:taskId', {
+           templateUrl: 'views/browse.html',
+            controller: 'BrowseController'
        })      
         .when('/register', {
            templateUrl: 'views/register.html',
