b534284d7beba79b7e1372445fd1974fd2716458
William Twachtman <wtguinness@aol.com>
2015-03-17 23:44:46 -0400

    +Split screen for description of selected Task +Allow Edit/Cancel only if you are the poster +Use task Service

--- scripts/controllers/taskController.js
+++ scripts/controllers/taskController.js
@@ -1,36 +1,31 @@
 'use strict';
-app.controller('TaskController', ['$scope', 'FURL', '$firebase', '$location', '$routeParams', 'toaster', function ($scope, FURL, $firebase, $location, $routeParams, toaster) {
-    var ref = new Firebase(FURL);
-    var fbTasks = $firebase(ref.child('tasks')).$asArray();
-    var taskId = $routeParams.taskId;
+app.controller('TaskController', ['$scope', '$location', 'toaster', 'Task', 'Auth', function ($scope, $location, toaster, Task, Auth) {
 
-    fbTasks.$loaded().then(function (data) {
-        $scope.tasks = fbTasks;
+    $scope.createTask = function () {
+        $scope.task.status = 'open';
+        $scope.task.gravatar = Auth.user.profile.gravatar;
+        $scope.task.name = Auth.user.profile.name;
+        $scope.task.poster = Auth.user.uid;
 
-        console.log("Length = " + fbTasks.length);
-    })
+        Task.createTask($scope.task).then(function (ref) {
+            toaster.pop('success', 'Task created successfully!');
+            $scope.task = {
+                title: '',
+                description: '',
+                total: '',
+                status: 'open',
+                gravatar: '',
+                name: '',
+                poster: ''
+            };
+            $location.path('/browse/' + ref.key());
+        });
+    };
+    $scope.editTask = function (task) {
+        Task.editTask(task).then(function () {
+            toaster.pop('success', 'Task is updated!');
+        });
 
-
-
-    if (taskId) {
-        $scope.selectedTask = getTask(taskId);
-    }
-
-
-    $scope.postTask = function (task) {
-        fbTasks.$add(task);
-        toaster.pop('success', 'Task is POSTED!');
-        $location.path('/browse');
-    }
-
-    function getTask(taskId) {
-        return $firebase(ref.child('tasks').child(taskId)).$asObject();
-    }
-
-    $scope.updateTask = function (task) {
-        $scope.selectedTask.$save(task);
-        toaster.pop('success', 'Task is UPDATED!');
-        $location.path('/browse');
-    }
+    };
 
 }]);
