23e79df7b2921a3311440f7cdc0e1eb95eb36ea1
William Twachtman <wtguinness@aol.com>
2015-03-25 19:01:19 -0400

    + Add Offer functionality with ability to 'Cancel' your offer.

--- scripts/controllers/browseController.js
+++ scripts/controllers/browseController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('BrowseController', ['$scope', '$routeParams', 'toaster', 'Task', 'Auth', 'Comment', function ($scope, $routeParams, toaster, Task, Auth, Comment) {
+app.controller('BrowseController', ['$scope', '$routeParams', 'toaster', 'Task', 'Auth', 'Comment', 'Offer', function ($scope, $routeParams, toaster, Task, Auth, Comment, Offer) {
     $scope.searchTask = '';
     $scope.tasks = Task.all;
     $scope.signedIn = Auth.signedIn;
@@ -18,10 +18,23 @@ app.controller('BrowseController', ['$scope', '$routeParams', 'toaster', 'Task',
     function setSelectedTask(task) {
         $scope.selectedTask = task;
         if ($scope.signedIn()) {
+
+            // Check if the current login user has already made an offer for selected task
+            Offer.isOffered(task.$id).then(function(data){
+                $scope.alreadyOffered = data;
+            });
+
             $scope.isTaskCreator = Task.isCreator;
             $scope.isOpen = Task.isOpen;
         }
+
         $scope.comments = Comment.comments(task.$id);
+
+        $scope.offers = Offer.offers(task.$id);
+
+        $scope.block = false;
+
+        $scope.isOfferMaker = Offer.isMaker;
     };
     $scope.cancelTask = function (taskId) {
         Task.cancelTask(taskId).then(function () {
@@ -41,4 +54,30 @@ app.controller('BrowseController', ['$scope', '$routeParams', 'toaster', 'Task',
             $scope.content = '';
         });
     };
+
+    $scope.makeOffer = function(){
+        var offer = {
+            total: $scope.total,
+            uid: $scope.user.uid,
+            name: $scope.user.profile.name,
+            gravatar: $scope.user.profile.gravatar
+        };
+
+        Offer.makeOffer($scope.selectedTask.$id, offer).then(function(){
+            toaster.pop('success', 'Your offer had been placed!');
+            $scope.total = '';
+            $scope.block = true;
+            $scope.alreadyOffered = true;
+        });
+    };
+
+    $scope.cancelOffer = function(offerId){
+        Offer.cancelOffer($scope.selectedTask.$id, offerId).then(function(){
+            toaster.pop('success', 'Your offer has been cancelled!');
+
+            $scope.alreadyOffered = false;
+            $scope.block = false;
+        });
+    };
+
 }]);
---
+++ scripts/services/offer.js
@@ -0,0 +1,53 @@
+'use strict';
+
+app.factory('Offer', ['FURL','$firebase', '$q', 'Auth', function(FURL, $firebase, $q, Auth){
+	
+	var ref = new Firebase(FURL);
+	var user = Auth.user;
+
+	var Offer = {
+
+		offers: function(taskId){
+			return $firebase(ref.child('offers').child(taskId)).$asArray();
+		},
+
+		makeOffer: function(taskId, offer){
+			var task_offers = this.offers(taskId);
+
+			if(task_offers){
+				return task_offers.$add(offer);
+			}
+		},
+	
+
+		isOffered: function(taskId){
+			if(user && user.provider){
+				var d = $q.defer();
+
+				$firebase(ref.child('offers').child(taskId).orderByChild("uid")
+					.equalTo(user.uid))
+					.$asArray()
+					.$loaded().then(function(data) {
+						d.resolve(data.length > 0);
+					}, function() {
+						d.reject(false);
+					});
+					return d.promise;
+			}
+		},
+
+		isMaker: function(offer){
+			return (user && user.provider && user.uid === offer.uid);
+		},
+
+		getOffer: function(taskId, offerId){
+			return $firebase(ref.child('offers').child(taskId).child(offerId));
+		},
+
+		cancelOffer: function(taskId, offerId){
+			return this.getOffer(taskId, offerId).$remove();
+		}
+	};
+
+	return Offer;
+}]);
